9fdfcc571e0484cebfe06df33fcceee249e5947b
github_ivorykeys87 <chase.r.catalano@gmail.com>
2025-03-10 19:48:05 -0400

    Added player object without moving capabilities

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 # this allows us to use code from the open-source pygame library throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    delta_time = 0
     while 1 == 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0), rect = None, special_flags = 0)
+        player.draw(screen)
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        delta_time = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
